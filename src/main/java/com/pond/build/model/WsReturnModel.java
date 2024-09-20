4c24a4af09d0aad0fef1ad41142fe728b5595ec2
“hollow288” <571339274@qq.com>
2024-09-20 09:31:10 +0800

    commit

---
+++ src/main/java/com/pond/build/model/WsReturnModel.java
@@ -0,0 +1,46 @@
+package com.pond.build.model;
+
+import java.util.Date;
+
+public class WsReturnModel {
+
+    String userId;
+
+    String content;
+
+    Date sendDate;
+
+    String messageType;
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Date getSendDate() {
+        return sendDate;
+    }
+
+    public void setSendDate(Date sendDate) {
+        this.sendDate = sendDate;
+    }
+
+    public String getMessageType() {
+        return messageType;
+    }
+
+    public void setMessageType(String messageType) {
+        this.messageType = messageType;
+    }
+}
